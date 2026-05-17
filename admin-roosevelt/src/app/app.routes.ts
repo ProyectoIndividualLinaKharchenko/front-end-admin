bc6e5d61932417ff55f5142c03f1dc5edb8d72d9
linakharchenko <lina.kharchenko@dxc.com>
2026-05-17 23:00:51 +0200

    wip navbar -- componentes admin

--- admin-roosevelt/src/app/app.routes.ts
+++ admin-roosevelt/src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { DashboardComponent } from './paginas/dashboard/dashboard.component';
+import { UsuariosComponent } from './paginas/usuarios/usuarios.component';
+import { MensajesComponent } from './paginas/mensajes/mensajes.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'usuarios', component: UsuariosComponent },
+  { path: 'mensajes', component: MensajesComponent },
+];
